fa492d689d3862f567e8163f10a3ba820713e705
umed <umed4878@gmail.com>
2021-07-26 20:06:19 +0500

    last commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+func IndexOfMaxAndMaxElement(numbers []int) (int, int) {
+	Index := 0
+	MaxElement := 0
+	for index, value := range numbers{
+		if MaxElement < value{
+		MaxElement = value
+		Index = index
+		}
+	}
+	return Index, MaxElement
+}
+
+func IndexOfMaxElement(numbers []int) int {
+	Index := 0
+	MaxElement := numbers[0]
+	for index, value := range numbers {
+		if MaxElement < value {
+			MaxElement = value
+			Index = index
+		}
+	}
+	return Index
+}
+
+func main() {
+IndexOfMaxElement([]int {10, -5, 1, -100, 25})
+}
---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIndexOfMaxAndMaxElement(t *testing.T) {
+	tests := []struct {
+		name  string
+		number []int
+		IndexWant  int
+		MaxElementWant int
+	}{
+		{"All of numbers is positive",  []int{1, 4, 25, 25,  100},  4,  100},
+		//{name: "All of numbers is negative", number: []int{1, -40, -10, -1, -5}, indexwant: 0, MaxElementwant: -1},
+		{ "Numbers has pos and neg",  []int{-1, 5, -20, 2},  1,  5},
+	}
+	for _, test := range tests {
+		index, element := IndexOfMaxAndMaxElement(test.number)
+		if index != test.IndexWant || element != test.MaxElementWant{
+			t.Errorf("IndexOfMaxAndElement test %s gotIndex %d Indexwant %d gotElement %d ElementWant %d",
+			test.name, index, test.IndexWant, element, test.MaxElementWant)
+		}
+
+	}
+}
